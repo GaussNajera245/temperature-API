5372f2d1ab5ecd94b041efda2915fff8f5e78eea
GusNajera <gvt_a9@hotmail.com>
2020-04-23 11:11:38 -0500

    front checkpoint

--- endpoint/index.js
+++ endpoint/index.js
@@ -6,8 +6,22 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('hello')
+io.on('connection', (sk)=>{
+    console.log('a user connected')
+});
+
+
+app.get('/', (req, res)=>{
+    res.send("hello")
+});
+
+app.post('/temp', (req, res) => {
+
+    console.log(req.body)
+
+    res.status(200).json({
+        message: "success"
+    })
 })
 
 http.listen( 8001, () => {
